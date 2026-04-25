a203feb14ce8cd0e8a783e311135527e03895a3f
Aaron Bentley <aaron@aaronbentley.com>
2026-04-25 18:54:50 -0400

    Finish initialization to the point of having a nix store.

--- runme.sh
+++ runme.sh
@@ -6,5 +6,12 @@ nixos-rebuild build-image --image-variant lxc-metadata --flake .#container
 meta=$(realpath result/tarball/nixos-image-lxc-metadata*)
 incus image import $meta $root --alias nixos-base
 incus launch -e nixos-base builder --config security.nesting=true
+incus storage volume create local flaked-incus
+incus config device add builder nix-store disk pool=local source=flaked-incus path=/mnt/nix-store
+incus exec builder -- bash -c "cd /nix/store && tar -cf - . | tar -xf - -C /mnt/nix-store"
+incus config device remove builder nix-store
+incus config device add builder nix-store disk pool=local source=flaked-incus path=/nix/store
+incus restart builder
 incus file push flake.nix builder/etc/nixos/flake.nix
 incus exec -- builder nixos-rebuild build --flake /etc/nixos#container
+incus stop builder
