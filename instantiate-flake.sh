1ed01b432d188a0040b50fc6680d946f61b15de5
Aaron Bentley <aaron@aaronbentley.com>
2026-04-26 17:08:08 -0400

    Simplify down to always-build, use-latest.

--- instantiate-flake.sh
+++ instantiate-flake.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 : ${target=$1}
 set -eux -o pipefail
-make_snapshot() {
-  incus storage volume snapshot create local flaked-incus $SNAPSHOT_HASH
-  incus storage volume snapshot create local flaked-incus latest --reuse
-}
 incus storage volume snapshot restore local flaked-incus latest
 incus launch nixos-base builder --config security.nesting=true
 incus config device add builder nix-dir disk pool=local source=flaked-incus path=/nix
@@ -12,12 +8,9 @@ incus file push flake.nix builder/etc/nixos/flake.nix
 incus file push flake.lock builder/etc/nixos/flake.lock
 sleep 5
 STORE_PATH=$(incus exec -- builder env XDG_CACHE_HOME=/nix/eval-cache nix build /etc/nixos#.nixosConfigurations.${target}.config.system.build.toplevel --print-out-paths --no-link)
-SNAPSHOT_HASH=$(echo -n "$STORE_PATH"|sha256sum|cut -c1-16)
-if ! incus storage volume snapshot show local flaked-incus/$SNAPSHOT_HASH &> /dev/null; then
-  make_snapshot
-fi
+incus storage volume snapshot create local flaked-incus latest --reuse
 incus rm --force builder
-incus storage volume copy local/flaked-incus/$SNAPSHOT_HASH local/flaked-incus-$target-workload
+incus storage volume copy local/flaked-incus/latest local/flaked-incus-$target-workload
 incus launch nixos-base $target
 incus config device add $target flaked-incus-$target-workload disk pool=local source=flaked-incus path=/nix
 sleep 5
