537b491d1887eebd66e0aeb59ca42e70f339bea6
Aaron Bentley <aaron@aaronbentley.com>
2026-04-26 13:27:24 -0400

    Always launch builder, calculate store path, use switch-to-configuration

--- instantiate-flake.sh
+++ instantiate-flake.sh
@@ -2,25 +2,24 @@
 : ${target=$1}
 set -eux -o pipefail
 make_snapshot() {
-  incus storage volume snapshot restore local flaked-incus latest
-  incus launch nixos-base builder --config security.nesting=true
-  incus config device add builder nix-dir disk pool=local source=flaked-incus path=/nix
-  sleep 5
-  incus file push flake.nix builder/etc/nixos/flake.nix
-  incus file push flake.lock builder/etc/nixos/flake.lock
   incus exec -- builder nixos-rebuild build --flake /etc/nixos#$target
-  incus rm --force builder
-  incus storage volume snapshot create local flaked-incus $target
+  incus storage volume snapshot create local flaked-incus $SNAPSHOT_HASH
   incus storage volume snapshot create local flaked-incus latest --reuse
 }
-
-if ! incus storage volume snapshot show local flaked-incus/$target &> /dev/null; then
+incus storage volume snapshot restore local flaked-incus latest
+incus launch nixos-base builder --config security.nesting=true
+incus config device add builder nix-dir disk pool=local source=flaked-incus path=/nix
+incus file push flake.nix builder/etc/nixos/flake.nix
+incus file push flake.lock builder/etc/nixos/flake.lock
+sleep 5
+STORE_PATH=$(incus exec builder -- nix eval /etc/nixos#nixosConfigurations.${target}.config.system.build.toplevel.outPath --raw)
+SNAPSHOT_HASH=$(echo -n "$STORE_PATH"|sha256sum|cut -c1-16)
+if ! incus storage volume snapshot show local flaked-incus/$SNAPSHOT_HASH &> /dev/null; then
   make_snapshot
 fi
-incus storage volume copy local/flaked-incus/$target local/flaked-incus-$target-workload
+incus rm --force builder
+incus storage volume copy local/flaked-incus/$SNAPSHOT_HASH local/flaked-incus-$target-workload
 incus launch nixos-base $target
 incus config device add $target flaked-incus-$target-workload disk pool=local source=flaked-incus path=/nix
-incus file push flake.nix $target/etc/nixos/flake.nix
-incus file push flake.lock $target/etc/nixos/flake.lock
 sleep 5
-incus exec -- $target nixos-rebuild switch --flake /etc/nixos#$target
+incus exec -- $target $STORE_PATH/bin/switch-to-configuration switch
